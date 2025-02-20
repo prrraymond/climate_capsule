7cdf0982bcedb77135acad4173b9533aa209abe4
Paul-Renaud Raymond <46355559+prray305@users.noreply.github.com>
2025-02-20 16:29:13 -0500

    Add 'use client' directive to components using hooks

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+'use client'
+import React, { useState } from 'react'
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Leaf, Car, Home, ShoppingBag, Shirt, Plus, ChevronDown, ChevronUp } from 'lucide-react';
 
