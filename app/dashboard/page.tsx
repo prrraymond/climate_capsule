89cceea8872a8474fd4b7dd56c3886a68382b3be
Paul-Renaud Raymond <46355559+prray305@users.noreply.github.com>
2025-02-20 16:37:07 -0500

    Add use client directive to page components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import React, { useState } from 'react'
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Leaf, Car, Home, ShoppingBag, Shirt, Plus, ChevronDown, ChevronUp } from 'lucide-react';
