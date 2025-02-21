444c122100b19d91d9d71a88aecc7904804bb3e4
Paul-Renaud Raymond <46355559+prray305@users.noreply.github.com>
2025-02-21 11:33:13 -0500

    Add utility functions for class name merging

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,9 @@
 // Utility functions for class names
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export default cn;
